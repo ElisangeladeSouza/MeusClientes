03819e2f4fc993aecfe65522193172a5c0142461
ElisangeladeSouza <elysangeladesouza@gmail.com>
2015-05-17 22:50:16 -0300

    Criação das telas principal e de cadastro de clientes.

--- app/src/main/java/android/curso/com/br/meusclientes/activity/MainActivity.java
+++ app/src/main/java/android/curso/com/br/meusclientes/activity/MainActivity.java
@@ -1,18 +1,35 @@
 package android.curso.com.br.meusclientes.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.curso.com.br.meusclientes.R;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private Button btClientes;
+    private Button btRelatorios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btClientes = (Button) findViewById(R.id.bt_clientes);
+
+        btClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ClienteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
