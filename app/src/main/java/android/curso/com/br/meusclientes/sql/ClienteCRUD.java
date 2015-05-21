2315b525ccbba524601db3e7cd4bddcc67227c05
ElisangeladeSouza <elysangeladesouza@gmail.com>
2015-05-21 19:41:32 -0300

    Criação da classe de banco SQLite e configuração para apresentar propagandas na aplicação

--- app/src/main/java/android/curso/com/br/meusclientes/sql/ClienteCRUD.java
+++ app/src/main/java/android/curso/com/br/meusclientes/sql/ClienteCRUD.java
@@ -1,7 +1,15 @@
 package android.curso.com.br.meusclientes.sql;
 
+import android.content.Context;
+
 /**
  * Created by root on 19/05/15.
  */
 public class ClienteCRUD {
+
+    private Context context;
+
+    public ClienteCRUD(Context context) {
+
+    }
 }
